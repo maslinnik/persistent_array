926d8fe2b87e0c4dd9c50934f40a05c6ec937dcc
maslinnik <pervutinskiy.ri@gmail.com>
2025-06-04 21:40:23 +0300

    Reformat files

--- tests/stress.cpp
+++ tests/stress.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
-#include "persistent_array.h"
 #include <random>
+#include "persistent_array.h"
 
 template <typename T, size_t N>
 struct SlowPersistentArray {
@@ -25,38 +25,39 @@ TEST(TestStress, SingleElement) {
   std::vector<SlowPersistentArray<int, 1>> slow = {{{0}}};
 
   for (int i = 0; i < ITERATIONS; ++i) {
-      int index = rnd() % (i + 1);
-      int new_val = rnd();
-      fast.push_back(fast[index].update(0, new_val));
-      slow.push_back(slow[index].update(0, new_val));
+    int index = rnd() % (i + 1);
+    int new_val = rnd();
+    fast.push_back(fast[index].update(0, new_val));
+    slow.push_back(slow[index].update(0, new_val));
   }
 
   for (int i = 0; i < ITERATIONS + 1; ++i) {
-      ASSERT_EQ(fast[i][0], slow[i][0]);
+    ASSERT_EQ(fast[i][0], slow[i][0]);
   }
 }
 
 TEST(TestStress, MultipleElements) {
-    const int ITERATIONS = 200'000;
-    const int N = 5;
+  const int ITERATIONS = 200'000;
+  const int N = 5;
 
-    std::mt19937 rnd{};
+  std::mt19937 rnd{};
 
-    std::array<int, N> initial{};
-    std::vector<persistent_array<int, N>> fast = {persistent_array<int, N>{initial.begin()}};
-    std::vector<SlowPersistentArray<int, N>> slow = {{initial}};
+  std::array<int, N> initial{};
+  std::vector<persistent_array<int, N>> fast = {
+      persistent_array<int, N>{initial.begin()}};
+  std::vector<SlowPersistentArray<int, N>> slow = {{initial}};
 
-    for (int i = 0; i < ITERATIONS; ++i) {
-        int index = rnd() % (i + 1);
-        int position = rnd() % N;
-        int new_val = rnd();
-        fast.push_back(fast[index].update(position, new_val));
-        slow.push_back(slow[index].update(position, new_val));
+  for (int i = 0; i < ITERATIONS; ++i) {
+    int index = rnd() % (i + 1);
+    int position = rnd() % N;
+    int new_val = rnd();
+    fast.push_back(fast[index].update(position, new_val));
+    slow.push_back(slow[index].update(position, new_val));
+  }
+
+  for (int i = 0; i < ITERATIONS + 1; ++i) {
+    for (int j = 0; j < N; ++j) {
+      ASSERT_EQ(fast[i][j], slow[i][j]);
     }
-
-    for (int i = 0; i < ITERATIONS + 1; ++i) {
-        for (int j = 0; j < N; ++j) {
-            ASSERT_EQ(fast[i][j], slow[i][j]);
-        }
-    }
+  }
 }
